da74fc77ac4c6425501ed6f9046d73ddab4d64b0
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2018-12-18 13:03:16 +0000

    tidied up version post route

--- module_repo/models/synthmoduleversion.model.js
+++ module_repo/models/synthmoduleversion.model.js
@@ -8,15 +8,7 @@ var SynthModuleVersionSchema = new Schema({
 	sockets: [{
 		isOutput: Boolean,
 		name: String,
-		channel: {
-			type: Number,
-			required: true,
-			unique: true,
-			validate: {
-				validator: Number.isInteger,
-				message: '{VALUE} is not an integer value'
-			}
-		}
+		channel: Number
 	}],
 	controls: [{
 		name: String
