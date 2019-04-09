6dab5743004bc9797c1e10a67fa749cd7760ec7e
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2019-04-09 10:08:42 +0100

    big speed boost from using ADC free-running mode

--- experiments/optimisation2/optimisation2.ino
+++ experiments/optimisation2/optimisation2.ino
@@ -2,6 +2,12 @@
 
 // this sketch is for optimizing the speed of the future polymod code
 
+// High when a value is ready to be read
+volatile int readFlag;
+
+// Value to store analog result
+volatile int analogVal;
+
 const int addressClock = 8;
 const int addressLatch = 9;
 const int addressData = 10;
@@ -23,15 +29,67 @@ void setup() {
   }
   pinMode(readConnections, INPUT_PULLUP);
   SPI.begin();
+
+  // clear ADLAR in ADMUX (0x7C) to right-adjust the result
+  // ADCL will contain lower 8 bits, ADCH upper 2 (in last two bits)
+  ADMUX &= B11011111;
+  
+  // Set REFS1..0 in ADMUX (0x7C) to change reference voltage to the
+  // proper source (01)
+  ADMUX |= B01000000;
+  
+  // Clear MUX3..0 in ADMUX (0x7C) in preparation for setting the analog
+  // input
+  ADMUX &= B11110000;
+  
+  // Set MUX3..0 in ADMUX (0x7C) to read from AD8 (Internal temp)
+  // Do not set above 15! You will overrun other parts of ADMUX. A full
+  // list of possible inputs is available in Table 24-4 of the ATMega328
+  // datasheet
+  ADMUX |= 0;
+  // ADMUX |= B00001000; // Binary equivalent
+  
+  // Set ADEN in ADCSRA (0x7A) to enable the ADC.
+  // Note, this instruction takes 12 ADC clocks to execute
+  ADCSRA |= B10000000;
+  
+  // Set ADATE in ADCSRA (0x7A) to enable auto-triggering.
+  ADCSRA |= B00100000;
+  
+  // Clear ADTS2..0 in ADCSRB (0x7B) to set trigger mode to free running.
+  // This means that as soon as an ADC has finished, the next will be
+  // immediately started.
+  ADCSRB &= B11111000;
+  
+  // Set the Prescaler to 128 (16000KHz/128 = 125KHz)
+  // Above 200KHz 10-bit results are not reliable.
+  ADCSRA |= B00000111;
+  
+  // Set ADIE in ADCSRA (0x7A) to enable the ADC interrupt.
+  // Without this, the internal interrupt will not trigger.
+  ADCSRA |= B00001000;
+  
+  // Enable global interrupts
+  // AVR macro included in <avr/interrupts.h>, which the Arduino IDE
+  // supplies by default.
+  sei();
+  
+  // Kick off the first ADC
+  readFlag = 0;
+  // Set ADSC in ADCSRA (0x7A) to start the ADC conversion
+  ADCSRA |=B01000000;
 }
 
 unsigned long lastStart = 0;
 unsigned long innerStart = 0;
 unsigned long innerEnd = 0;
-int numGroups = 8;
+int numGroups = 2;
+//int analogValues[8*8*8];
+byte cyclesSinceRead[8*8*8];
 
 void loop() {
   Serial.println("START");
+  bool thingFailed = false;
   lastStart = millis();
   int shiftData = 0; // 2-byte value to send to shift register
   for(byte a=0;a<numGroups;a++) {
@@ -115,13 +173,29 @@ void loop() {
                 // maybe put analogRead stuff here?
                 
                 if(!bitRead(PINC,2)) {
-                  //Serial.print(socket1);
-                  //Serial.print("->");
-                  //Serial.println(socket2);
+                  Serial.print(socket1);
+                  Serial.print("->");
+                  Serial.println(socket2);
                 }
               }
 
-              int testReading = analogRead(A0);
+              // do analog stuff here for now
+              if(readFlag == 1) {
+                //analogValues[socket2] = analogVal;
+                cyclesSinceRead[socket2] = 0;
+                readFlag = 0;
+                int testVal = analogVal;
+              } else {
+                cyclesSinceRead[socket2]++;
+                if(cyclesSinceRead[socket2] > 10) {
+                  while(readFlag != 1) {
+                    // wait
+                  }
+                  cyclesSinceRead[socket2] = 0;
+                  readFlag = 0;
+                  int testVal = analogVal;
+                }
+              }
             }
           }
         }
@@ -133,5 +207,19 @@ void loop() {
   Serial.println(innerEnd - innerStart);
   Serial.print("TOTAL TIME: ");
   Serial.println(millis() - lastStart);
+  Serial.println(thingFailed?"FAILED":"SUCCEEDED");
 }
 
+// Interrupt service routine for the ADC completion
+ISR(ADC_vect){
+
+  // Done reading
+  readFlag = 1;
+  
+  // Must read low first
+  analogVal = ADCL | (ADCH << 8);
+  
+  // Not needed because free-running mode is enabled.
+  // Set ADSC in ADCSRA (0x7A) to start another ADC conversion
+  // ADCSRA |= B01000000;
+}
