c78f0033cac09f2ca568e545730fda3841e4b7d3
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2020-09-03 17:21:37 +0100

    made a mess but heading in right direction

--- experiments/polymodnano/mainboardnew/mainboardnew.ino
+++ experiments/polymodnano/mainboardnew/mainboardnew.ino
@@ -4,20 +4,25 @@ int latchPin = 8;
 int clockPin = 10;
 int dataPin = 9;
 
+const int maxModules = 8;
+int moduleTypes[maxModules];
+byte tickNum = 0;
+
 void setup()
 {
+  while(!Serial);
+  Serial.begin(9600);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(13, OUTPUT);
-  Serial.begin(9600);
   Serial.println("polymod main board started");
   Wire.begin(); // join i2c bus (address optional for master)
   digitalWrite(latchPin, LOW);
   shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
   digitalWrite(latchPin, HIGH);
   delay(5000); // allow modules to power up
-  for(int i=0;i<8;i++) {
+  for(int i=0;i<maxModules;i++) {
     byte thisByte = 0;
     bitWrite(thisByte,i,HIGH);
     for(int j=0; j<i+1; j++) {
@@ -32,20 +37,69 @@ void setup()
     }
   }
   delay(3000);
-  for(int i=0; i<8; i++) {
+  for(int i=0; i<maxModules; i++) {
+    moduleTypes[i] = 0; // default to type 0 (no module connected)
+    Wire.beginTransmission(i+1); // broadcast to all modules
+    Wire.write(1); // message type 1 (module type request)
+    Wire.endTransmission();    // stop transmitting
     Wire.requestFrom(i+1,1);
     while(Wire.available()) {
-      byte thisModuleType = Wire.read();
+      moduleTypes[i] = Wire.read();
       Serial.print("slot ");
       Serial.print(i);
       Serial.print(" type ");
-      Serial.println(thisModuleType);
+      Serial.println(moduleTypes[i]);
     }
   }
   Serial.println("end of setup");
 }
 
+byte thisMessage[8]; // max message length currently 5 but giving some wiggle room
+byte messageLengths[] = {2,1,3,3,3,3,5,5};
 void loop()
 {
-  
+  Wire.beginTransmission(0); // broadcast to all modules
+  Wire.write(0); // message type 0 (tick)
+  Wire.write(tickNum);
+  Wire.endTransmission();    // stop transmitting
+
+  tickNum++;
+
+  if(tickNum == 32) {
+    // on last tick, request data from all modules
+    for(byte n=0; n<=maxModules; n++) {
+      Wire.requestFrom(n+1,1); // request data
+      byte bytesExpected = 0;
+      while(Wire.available()) {
+        bytesExpected = Wire.read();        
+      }
+      byte byteNum = 0;
+      Wire.requestFrom(n+1,bytesExpected);
+      while(Wire.available()) {
+        thisMessage[byteNum] = Wire.read();
+        byteNum ++;
+        if(byteNum >= messageLengths[thisMessage[0]]) {
+          // message complete, handle command
+          switch(thisMessage[0]) {
+            case 6:
+            Serial.println("connection");
+            break;
+
+            case 7:
+            Serial.println("disconnection");
+            break;
+
+            case 2:
+            Serial.print("analog value: ");
+            Serial.println(thisMessage[2]);
+          }
+          byteNum = 0;
+        }
+      }
+    }
+    tickNum = 0;
+    delayMicroseconds(10);
+  }
+
+  delayMicroseconds(10);
 }
--- experiments/polymodnano/moduleboardnew/moduleboardnew.ino
+++ experiments/polymodnano/moduleboardnew/moduleboardnew.ino
@@ -4,14 +4,31 @@
 byte idPin = 10;
 byte ledPin = 13;
 int moduleNum = 0;
-int moduleType = 227; // will be different for a VCO, LFO, etc
+int moduleType = 12; // will be different for a VCO, LFO, etc
 unsigned long lastRead = 0;
 bool foundModuleNum = false;
+const byte outPins[] = {2,3,4,5};
+const byte inPins[] = {6,7,8,9};
+byte prevConnections[4][2];
+byte newConnections[4][2];
+byte prevConfirmedConnections[4][2];
+byte confirmedConnections[4][2];
+byte unchangedCount[4];
+bool sendConnection[4];
+bool sendDisconnection[4];
+byte everConnected[4];
 
 Bounce b = Bounce();
 
 void setup() {
+  while(!Serial);
   Serial.begin(9600);
+  for(byte i=0;i<4;i++) {
+    pinMode(outPins[i], OUTPUT);
+    pinMode(inPins[i], INPUT_PULLUP);
+    prevConnections[i][0] = 255;
+    prevConnections[i][1] = 255;
+  }
   b.attach(idPin, INPUT);
   b.interval(5);
   pinMode(ledPin, OUTPUT);
@@ -42,21 +59,150 @@ void loop() {
 byte byteNum = 0;
 byte message[2];
 byte tickNum = 0;
+byte messageLengths[] = {2,1,3,3,3,3,5,5};
 
 void receiveEvent(int howMany) {
   while(Wire.available()) {
     message[byteNum] = Wire.read();
     byteNum ++;
-    if(byteNum == 2) {
-      if(message[0] == 0) {
+    if(byteNum >= messageLengths[message[0]]) {
+      // message complete, handle command
+      switch(message[0]) {
+        case 0:
         tickNum = message[1];
-        //doTick();
+        doTick();
       }
       byteNum = 0;
     }
   }
 }
 
+bool numBytesSent = false;
 void requestEvent() {
-  Wire.write(moduleType);
+  if(message[0]==1) {
+    // send module type
+    Wire.write(moduleType);
+    Serial.println("sending module type");
+  } else if(message[0]==0) {
+    // send changed data
+    byte numNewConnected = 0;
+    byte numNewDisconnected = 0;
+    for(byte i=0; i<4; i++) {
+      if(sendConnection[i]) {
+        numNewConnected ++;
+      }
+      if(sendDisconnection[i]) {
+        numNewDisconnected ++;
+      }
+    }
+    if(!numBytesSent) {
+      Wire.write(3); // sending 3 bytes for testing
+      numBytesSent = true;
+    } else {
+      Wire.write(2);
+      Wire.write(0);
+      int testAnalogValue = analogRead(0) / 4;
+      Wire.write(testAnalogValue);
+      numBytesSent = false;
+    }
+    //Wire.write(numNewConnected);
+    //Wire.write(numNewDisconnected);
+    for(byte i=0; i<4; i++) {
+      if(sendConnection[i]) {
+        /*Wire.write(6); // connection message
+        Wire.write(confirmedConnections[i][0]);
+        Wire.write(confirmedConnections[i][1]);
+        Wire.write(moduleNum);
+        Wire.write(i);*/
+        sendConnection[i] = false;
+      }
+    }
+    for(byte i=0; i<4; i++) {
+      if(sendDisconnection[i]) {
+        /*Wire.write(7); // disconnection message
+        Wire.write(prevConfirmedConnections[i][0]);
+        Wire.write(prevConfirmedConnections[i][1]);
+        Wire.write(moduleNum);
+        Wire.write(i);*/
+        sendDisconnection[i] = false;
+      }
+    }
+  }
+  
+}
+
+void doTick() {
+  if(tickNum==0) {
+    for(byte i=0; i<4; i++) {
+      newConnections[i][0] = 255;
+      newConnections[i][1] = 255;
+    }
+  }
+  byte phaseIndex = tickNum/2;
+  if(tickNum%2==0) {
+    // write phase
+    for(byte i=0; i<4; i++) {
+      if(phaseIndex<8) {
+        digitalWrite(outPins[i], bitRead(moduleNum, phaseIndex));
+      } else {
+        digitalWrite(outPins[i], bitRead(i, phaseIndex - 8));
+      }
+    }
+  } else {
+    // read phase
+    for(byte i=0; i<4; i++) {
+      if(phaseIndex<8) {
+        bitWrite(newConnections[i][0], phaseIndex, digitalRead(inPins[i]));
+      } else {
+        bitWrite(newConnections[i][1], phaseIndex - 8, digitalRead(inPins[i]));
+      }
+    }
+  }
+  if(tickNum==31) {
+    for(byte i=0; i<4; i++) {
+      if(newConnections[i][0] != prevConnections[i][0] || newConnections[i][1] != prevConnections[i][1]) {
+        prevConnections[i][0] = newConnections[i][0];
+        prevConnections[i][1] = newConnections[i][1];
+        unchangedCount[i] = 0;
+      } else {
+        if(unchangedCount[i] < 3) {
+          unchangedCount[i] ++;
+        }
+        if(unchangedCount[i] == 2) {
+          prevConfirmedConnections[i][0] = confirmedConnections[i][0];
+          prevConfirmedConnections[i][1] = confirmedConnections[i][1];
+          confirmedConnections[i][0] = newConnections[i][0];
+          confirmedConnections[i][1] = newConnections[i][1];
+          if(confirmedConnections[i][0]==255) {
+            if(everConnected[i]) {
+              Serial.print("disconnect ");
+              Serial.print("m");
+              Serial.print(prevConfirmedConnections[i][0], DEC);
+              Serial.print("p");
+              Serial.print(prevConfirmedConnections[i][1], DEC);
+              Serial.print("->m");
+              Serial.print(moduleNum, DEC);
+              Serial.print("p");
+              Serial.print(i, DEC);
+              Serial.println("");
+              sendDisconnection[i] = true;
+            }
+          } else {
+            Serial.print("connect ");
+            Serial.print("m");
+            Serial.print(confirmedConnections[i][0], DEC);
+            Serial.print("p");
+            Serial.print(confirmedConnections[i][1], DEC);
+            Serial.print("->m");
+            Serial.print(moduleNum, DEC);
+            Serial.print("p");
+            Serial.print(i, DEC);
+            Serial.println("");
+            sendConnection[i] = true;
+            everConnected[i] = true;
+          }
+        }
+      }
+    }
+  }
 }
